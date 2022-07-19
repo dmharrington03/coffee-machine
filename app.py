88d17551841a4e3db319ee3f3f2ca32c4ad7b544
Daniel Harrington <dmharrington03@gmail.com>
2022-07-19 10:00:40 -0400

    progress routes, time remaining

--- app.py
+++ app.py
@@ -45,10 +45,14 @@ def index():
                 return render_template('index.html', form=form)
 
             if timeDiff < timedelta(minutes=5):
-                    return render_template('inProgress.html', remaining=round(timeDiff.total_seconds()))
+                return render_template('inProgress.html', remaining=round(timeDiff.total_seconds()))
+            else:
+                # Show listed alarm
+                return render_template('alarm.html', time=brewTime.strftime('%I:%M %p'))
 
         return render_template('index.html', form=form)
 
+    # POST
     else:
         inputTime = form.time.data
         currentTime = datetime.now()
@@ -64,12 +68,43 @@ def index():
         timeScheduler.enterabs(time.mktime(brewTime.timetuple()), priority=1, action=print, argument=(brewTime,))
         startThread = threading.Thread(target=timeScheduler.run)
         startThread.start()
-        
-        return render_template('inProgress.html', form=form)
 
-        
+        timeDiff = brewTime - datetime.now()
+        if timeDiff < timedelta(minutes=5):
+            return render_template('inProgress.html', remaining=round(timeDiff.total_seconds()))
+        else:
+            # Alarm Set
+            return render_template('alarm.html', time=brewTime.strftime('%I:%M %p'))
+
+
+@app.route('/clear')
+def clear():
+    db = sqlite3.connect("./times.db")
+    dbcur = db.cursor()
+    dbcur.execute("DELETE FROM times")
+    db.commit()
+    db.close()
+    form = CoffeeForm()
+    return render_template('index.html', form=form)
         
 
+@app.route('/progress')
+def progress():
+    db = sqlite3.connect("./times.db")
+    dbcur = db.cursor()
+    res = dbcur.execute("SELECT * FROM times").fetchall()[0][0]
+    timeDiff = datetime.fromtimestamp(res) - datetime.now()
+    return str(round(timeDiff.total_seconds())) # Seconds remaining
+
+
+
+#TODO return redirect
+@app.route('/stop')
+def stop():
+    form = CoffeeForm()
+    return render_template('index.html', form=form)
+
+#TODO return redirect
 @app.route('/brew')
 def brew():
     
