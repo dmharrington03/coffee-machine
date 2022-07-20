2adffcd937c667270c10ea593230b0ca6b931e91
Daniel Harrington <dmharrington03@gmail.com>
2022-07-19 21:02:06 -0400

    finished all routes

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template,  url_for, redirect, request
+import math
+from flask import Flask, render_template, redirect, url_for, request
 # from requests import request
 from form import CoffeeForm
 import sched, threading, time
@@ -13,6 +14,7 @@ pin = 18
 timeScheduler = sched.scheduler(time.time, time.sleep)
 
 # relay = OutputDevice(pin)
+BREW_SECS = 1 * 60 # 5 minutes
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
@@ -37,15 +39,19 @@ def index():
             print(f"Stored time {res}")
             brewTime = datetime.fromtimestamp(res)
 
-            timeDiff = brewTime - datetime.now()
-            if timeDiff.total_seconds() <= 0:
+            timeDiff = (datetime.now() - brewTime).total_seconds()
+            # Done Brewing
+            if timeDiff>= BREW_SECS:
                 dbcur.execute("DELETE FROM times")
                 db.commit()
                 # Return DONE BREWING
                 return render_template('index.html', form=form)
 
-            if timeDiff < timedelta(minutes=5):
-                return render_template('inProgress.html', remaining=round(timeDiff.total_seconds()))
+            # Brewing in Progress
+            elif (timeDiff < BREW_SECS) and (timeDiff > 0):
+                return render_template('inProgress.html')
+            
+            # Brewing not in Progress
             else:
                 # Show listed alarm
                 return render_template('alarm.html', time=brewTime.strftime('%I:%M %p'))
@@ -70,22 +76,35 @@ def index():
         startThread.start()
 
         timeDiff = brewTime - datetime.now()
-        if timeDiff < timedelta(minutes=5):
-            return render_template('inProgress.html', remaining=round(timeDiff.total_seconds()))
-        else:
-            # Alarm Set
-            return render_template('alarm.html', time=brewTime.strftime('%I:%M %p'))
+
+        return render_template('alarm.html', time=brewTime.strftime('%I:%M %p'))
 
 
-@app.route('/clear')
-def clear():
+@app.route('/brew')
+def brew():
+    db = sqlite3.connect("./times.db")
+    dbcur = db.cursor()
+    dbcur.execute("DELETE FROM times")
+    value = (int(math.floor(time.time())),)
+    dbcur.execute("INSERT INTO times (time) VALUES (?)", value)
+    db.commit()
+    db.close()
+    form = CoffeeForm()
+    # Call brew()
+    return redirect("/")
+
+@app.route('/finished')
+def finished():
+    return render_template('finished.html')
+
+@app.route('/delete')
+def delete():
     db = sqlite3.connect("./times.db")
     dbcur = db.cursor()
     dbcur.execute("DELETE FROM times")
     db.commit()
     db.close()
-    form = CoffeeForm()
-    return render_template('index.html', form=form)
+    return redirect("/")
         
 
 @app.route('/progress')
@@ -93,22 +112,20 @@ def progress():
     db = sqlite3.connect("./times.db")
     dbcur = db.cursor()
     res = dbcur.execute("SELECT * FROM times").fetchall()[0][0]
-    timeDiff = datetime.fromtimestamp(res) - datetime.now()
-    return str(round(timeDiff.total_seconds())) # Seconds remaining
+    db.close()
+    timeDiff = (datetime.now() - datetime.fromtimestamp(res)).total_seconds()
+    if (timeDiff < BREW_SECS) and (timeDiff > 0): # If brewing in progress, should always be called
+        return str(math.floor(timeDiff)) # Seconds elapsed
+    else: # Alarm not yet gone off, should never be called
+        return ""
 
 
 
-#TODO return redirect
 @app.route('/stop')
 def stop():
     form = CoffeeForm()
     return render_template('index.html', form=form)
 
-#TODO return redirect
-@app.route('/brew')
-def brew():
-    
-    return render_template('inProgress.html', remaining=300)
 
 
 
