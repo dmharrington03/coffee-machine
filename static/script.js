abf526c6bb20b01a7fbfe335b07eaf9c8955fcd0
Daniel Harrington <dmharrington03@gmail.com>
2022-07-19 21:01:49 -0400

    progress bar functionality

--- static/script.js
+++ static/script.js
@@ -1,5 +1,16 @@
 setInterval(() => {
   fetch("/progress")
-  .then((res) => console.log(res.text()))
+  .then((res) => {
+    res.text()
+    .then((text) => {
+      let prog = 60.0 - parseFloat(text);
+      if (text == '' || prog <= 0.0)
+        document.location.href = "/finished"
+      let w = 100 - Math.floor((prog / 60) * 100)
+      document.getElementById('bar').style.width = w + '%';
+      document.getElementById('perc').innerHTML = w + '%';
+    })
+  })
   .catch((err) => console.log(err))
-}, 5000)
+}, 1000)
+
