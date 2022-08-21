af67989c4acea585b9e5ce576494b761ab9ef823
Daniel Harrington <dmharrington03@gmail.com>
2022-08-20 21:37:07 -0400

    uncommented rpi code

--- brew.py
+++ brew.py
@@ -1,19 +1,17 @@
-import sched, time
-# import RPi.GPIO as GPIO
+import time
+import RPi.GPIO as GPIO
 
 def startBrew(pin: int, secs: int, stopFunc):
-#   GPIO.output(pin, GPIO.HIGH)
+    GPIO.output(pin, GPIO.HIGH)
     print("STARTING BREW - GPIO HIGH")
     for i in range(secs):
         if stopFunc():
             return
         time.sleep(1)
-#   GPIO.output(pin, GPIO.LOW)
+    GPIO.output(pin, GPIO.LOW)
     print("STOPPING BREW - GPIO LOW")
-    # pass
 
 
 def stopBrew(pin):
-#   GPIO.output(pin, GPIO.LOW)
+    GPIO.output(pin, GPIO.LOW)
     print("STOPPING BREW ON STOP - GPIO LOW")
-    # pass
